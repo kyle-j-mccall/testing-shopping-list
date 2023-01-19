1d013676cec852bce64e960f5d1c62daff653cdf
Kyle McCall <kylem147@gmail.com>
2023-01-19 15:34:04 -0800

    shopping list prop passing

--- src/components/ShoppingList/ShoppingListsTest.test.js
+++ src/components/ShoppingList/ShoppingListsTest.test.js
@@ -1,14 +1,22 @@
 import { screen, render } from '@testing-library/react';
-import ShoppingListForm from './ShoppingListForm';
 import ShoppingLists from './ShoppingLists';
 
-describe('Shopping list form', () => {
-  const shoppingList = {
-    id: 1,
-    created_at: 'today',
-    name: 'Kyles List',
-  };
-  render(<ShoppingLists shoppingLists={shoppingList} />);
+describe('Shopping lists', () => {
+  it('renders shopping lists', () => {
+    const shoppingList = [
+      {
+        id: 1,
+        created_at: 'today',
+        name: 'Kyles List',
+        shoppingItems: [
+          {
+            item: 'bananas',
+          },
+        ],
+      },
+    ];
+    render(<ShoppingLists shoppingLists={shoppingList} />);
 
-  expect(screen.getByTestId('shopping-lists')).not.toBe(null);
+    expect(screen.getByTestId('shopping-list-1')).not.toBe(null);
+  });
 });
