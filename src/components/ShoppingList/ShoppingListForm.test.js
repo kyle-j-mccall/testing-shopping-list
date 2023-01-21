c0f3f589c5448ae2893a896873bd8b3125544368
Kyle McCall <kylem147@gmail.com>
2023-01-20 17:54:16 -0800

    form value changed on submit test passing

--- src/components/ShoppingList/ShoppingListForm.test.js
+++ src/components/ShoppingList/ShoppingListForm.test.js
@@ -1,21 +1,8 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 
 import ShoppingListForm from './ShoppingListForm';
 
 describe('shopping list form', () => {
-  const shoppingList = [
-    {
-      id: 1,
-      created_at: 'today',
-      name: 'Kyles List',
-      shoppingItems: [
-        {
-          item: 'bananas',
-        },
-      ],
-    },
-  ];
-
   it('form renders a submit button', () => {
     render(<ShoppingListForm id="1" />);
 
@@ -23,5 +10,36 @@ describe('shopping list form', () => {
     expect(
       screen.getByTestId('shopping-list-form-submit-button-1')
     ).not.toBe(null);
+  });
+  it('form value is changed on on submit', () => {
+    const shoppingList = {
+      id: null,
+      name: '',
+      shoppingItems: [],
+    };
+    const onSubmitItem = jest.fn();
+    render(
+      <ShoppingListForm
+        id="1"
+        onSubmit={onSubmitItem}
+        shoppingList={shoppingList}
+      />
+    );
+
+    fireEvent.change(
+      // eslint-disable-next-line max-len
+      screen.getByTestId('shopping-list-form-name-1'),
+      { target: { value: 'test' } }
+    );
+
+    fireEvent.click(
+      screen.getByTestId('shopping-list-form-submit-button-1')
+    );
+
+    // eslint-disable-next-line max-len
+    expect(onSubmitItem).toHaveBeenCalledWith({
+      ...shoppingList,
+      name: 'test',
+    });
   });
 });
